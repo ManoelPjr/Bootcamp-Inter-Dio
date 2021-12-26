8c37dc2bfb5c7d97c865ea70278a4cc86a208b7d
ManoelPjr <napolesjr@gmail.com>
2021-12-26 09:58:22 -0300

    Manipulando arquivos - Variáveis e Constantes

---
+++ Introdução ao JavaScript/teste.js
@@ -0,0 +1,8 @@
+//var x = 0; Comentário de uma linha
+/*var x = 0; Comentário de várias linhas
+var y = 1;*/ 
+var total = 0.00; // Variáveis podem ser alteradas em runtime
+var desconto = 0.20;
+const PRECO = 2.00; //Constantes Não podem ser alteradas em runtime
+
+total = PRECO - desconto;
