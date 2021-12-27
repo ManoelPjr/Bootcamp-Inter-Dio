3c1f84a9f155272ecd92877034e227816b792863
ManoelPjr <napolesjr@gmail.com>
2021-12-27 11:22:15 -0300

    Criando um contador

--- Introdução ao JavaScript/assets/js/scripts.js
+++ Introdução ao JavaScript/assets/js/scripts.js
@@ -1,1 +1,22 @@
-console.log("Helo world");
+var currentNumberWrapper = document.getElementById("currentNumber");
+var currentNumber = 0;
+
+function colorChange () {
+    if(currentNumber <0){
+        currentNumberWrapper.style.color = '#FF0000';
+    }else{
+        currentNumberWrapper.style.color = '#000000';
+    } 
+      
+}
+function increment(){
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+    colorChange();
+}
+
+function decrement(){
+    currentNumber = currentNumber - 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+    colorChange();
+}
