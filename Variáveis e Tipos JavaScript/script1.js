9c68103d4d438873c4e30946f19593d37e21c919
ManoelPjr <napolesjr@gmail.com>
2022-01-05 11:41:28 -0300

    Constantes JS

---
+++ Variáveis e Tipos JavaScript/script1.js
@@ -0,0 +1,10 @@
+// Constantes devem ser declaradas usando UPPER_SNAKE_CASE
+// Constantes não podem ser redeclaradas;
+const FIRST_NAME = 'Manoel';
+const LAST_NAME  = 'Pedro';
+
+// if (FIRST_NAME === 'Manoel'){
+//     FIRST_NAME = 'João'; //Gera erro pois constantes não podem ser alteradas
+// }
+//const FIRST_NAME; //Gera erro por tentativa de redeclaração da constante
+console.log(FIRST_NAME, LAST_NAME);
