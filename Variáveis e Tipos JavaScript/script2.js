46e56825c6437d918dff63594b2f6e8e3c7843cd
ManoelPjr <napolesjr@gmail.com>
2022-01-05 17:09:02 -0300

    Tipos primitivos

---
+++ Variáveis e Tipos JavaScript/script2.js
@@ -0,0 +1,17 @@
+//tipos primitivos
+let variavelUm = 'Manoel';//String
+let variavelDois = true;//boolean/
+let variavelTres = 51;//number
+let variavelQuatro;//undefined
+console.log("< ----------------- >");
+console.log(variavelUm);
+console.log(typeof(variavelUm));
+console.log("< ----------------- >");
+console.log(variavelDois);
+console.log(typeof(variavelDois));
+console.log("< ----------------- >");
+console.log(variavelTres);
+console.log(typeof(variavelTres));
+console.log("< ----------------- >");
+console.log(variavelQuatro);
+console.log(typeof(variavelQuatro));
